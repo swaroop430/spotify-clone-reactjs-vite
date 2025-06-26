eb07d6be6f32e214b2f17299fa0ce77d2cbf4abd
Swaroop <jyothiswaroopdanduboyina@gmail.com>
2025-06-26 19:49:42 +0530

    Updated song data in data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -52,14 +52,7 @@ export const playlists: Playlist[] = [
     cover: "https://f4.bcbits.com/img/a2793859494_16.jpg",
     artists: ["Chau Saura", "amies", "kyu"],
   },
-  {
-    id: "6",
-    albumId: 6,
-    title: "Like a Necessity",
-    color: colors.orange,
-    cover: "https://f4.bcbits.com/img/a0363730459_16.jpg",
-    artists: ["WFS", "Nadav Cohen"],
-  },
+  
 ];
 
 export const morePlaylists = playlists.map((item) => ({
